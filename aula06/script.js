a906b5fc680c3767e30bc3249dd4239b7f6588f8
Claudio Gomes <143952529+claudimes@users.noreply.github.com>
2024-03-25 01:25:29 -0300

    Aula_10

--- aula06/script.js
+++ aula06/script.js
@@ -13,7 +13,9 @@ d.innerText = 'Olá'*/
 //d.style.background = 'blue'
 var d = window.document.querySelector("div#msg")
 d.style.background = 'blue'
-var h = document.getElementById('h1')
+//var h = document.getElementById('h1')
+var h = document.querySelector("h1.h1")
 h.innerHTML = 'Aqui é o JavaCript mandando ver'
+h.style.background = 'yellowgreen'
 h.style.textAlign = 'center'
 
