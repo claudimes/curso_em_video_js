c223d9c2f166ec5ceb7afced88330aea29412f32
Claudio Gomes <143952529+claudimes@users.noreply.github.com>
2024-03-24 21:08:57 -0300

    Criando_ArquivoJS

---
+++ aula06/script.js
@@ -0,0 +1,15 @@
+//window.document.write('Ola Mundo!')
+var corpo = document.body
+var p1 = document.getElementsByTagName('p')[1]
+//var d = document.getElementsByName('msg')[0]
+//corpo.style.background = 'black'
+document.write(p1.innerHTML)
+/*alert(p1.innerText)
+p1.style.color = 'blue'
+d.style.background = 'green'
+document.write('Está escrito assim: ' + p1.innerText)
+d.innerText = 'Olá'*/
+//var d = window.document.querySelector("div#msg")
+//d.style.background = 'blue'
+var d = window.document.querySelector("div#msg")
+d.style.background = 'blue'
